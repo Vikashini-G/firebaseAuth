b6c9291152d1494de07d34f16890568939984e4d
Vikashini G <112184188+Vikashini-G@users.noreply.github.com>
2024-03-03 18:29:52 +0530

    Initial Commit

---
+++ checkFirebase/checkFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkFirebaseApp.swift
+//  checkFirebase
+//
+//  Created by Vikashini G on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct checkFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
